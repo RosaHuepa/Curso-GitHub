b008248912cf77989aad6cc527e02960fc880aee
RosaHuepa <rosahuepa.150@gmail.com>
2024-03-04 23:50:59 -0600

    usamos los dos comandos en uno add y commit

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+// script.js
+//Rosa Huepa 
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Tu código JavaScript aquí, se ejecutará cuando la página esté completamente cargada.
+    console.log('¡Hola desde JavaScript!');
+});
